6f02b0203e5ee3cc3531824b3e299120600120a6
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2025-08-16 18:27:57 -0300

    Revert "mongo"
    This reverts commit 4462822b41f020f27f65db4a4730ccd6d10b4d4d.

--- src/main/java/com/jpdev01/rinha/repository/PagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jpdev01.rinha.repository;
-
-import com.jpdev01.rinhadebackend.entity.PagamentoDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PagamentoRepository extends MongoRepository<PagamentoDocument, String> {
-}
